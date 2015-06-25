fe28b37666c3db68432cdfbbd536b87997eed5ff
Stefan Schwager <stefan.schwager@axelspringer.de>
2015-06-25 12:59:29 +0200

    implements shy state

--- State/State/Shy.php
+++ State/State/Shy.php
@@ -12,18 +12,31 @@ use Pattern\State\StudentStateInterface;
 
 class Shy implements StudentStateInterface
 {
+    /**
+     * @var Student
+     */
+    private $student;
+
+    public function __construct(Student $student)
+    {
+        $this->student = $student;
+    }
+
     public function spendBeer()
     {
-        // TODO: Implement spendBeer() method.
+        echo 'Yes, give me more!';
+        $this->student->setCurrentState(new Drunken($this->student));
     }
 
     public function spendCannabis()
     {
-        // TODO: Implement spendCannabis() method.
+        echo 'Year, Peace my friend';
+        $this->student->setCurrentState(new Stoned($this->student));
     }
 
     public function spendWater()
     {
-        // TODO: Implement spendWater() method.
+        echo 'Pfff, give me a beer!';
+        $this->student->setCurrentState($this);
     }
 }
