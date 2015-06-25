0685864494218e5f929c8c3c1611f45e631c0879
Stefan Schwager <stefan.schwager@axelspringer.de>
2015-06-25 13:09:26 +0200

    Implementation of the stoned state

--- State/State/Stoned.php
+++ State/State/Stoned.php
@@ -12,18 +12,31 @@ use Pattern\State\StudentStateInterface;
 
 class Stoned implements StudentStateInterface
 {
+    /**
+     * @var Student
+     */
+    private $student;
+
+    public function __construct(Student $student)
+    {
+        $this->student = $student;
+    }
+
     public function spendBeer()
     {
-        // TODO: Implement spendBeer() method.
+        echo 'Give me something to eat and not a beer.';
+        $this->student->setCurrentState($this);
     }
 
     public function spendCannabis()
     {
-        // TODO: Implement spendCannabis() method.
+        echo 'Yes give me more pot';
+        $this->student->setCurrentState(new Crash($this->student));
     }
 
     public function spendWater()
     {
-        // TODO: Implement spendWater() method.
+        echo 'Thanks, I have a dry mouth';
+        $this->student->setCurrentState($this);
     }
 }
