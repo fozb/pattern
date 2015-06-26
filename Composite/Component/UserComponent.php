661dd630589604e458adb5fdf3df0b7b616db257
Stefan Schwager <stefan.schwager@axelspringer.de>
2015-06-26 13:00:49 +0200

    add empty user component

---
+++ Composite/Component/UserComponent.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @category    Pattern
+ * @package     Composite
+ * @subpackage  Component
+ * @author      Stefan Schwager
+ */
+
+namespace Pattern\Composite\Component;
+
+abstract class UserComponent
+{
+}
