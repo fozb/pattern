e2fd193f3fb30ee051c1889cc9068bafd9f87899
Stefan Schwager <stefan.schwager@axelspringer.de>
2015-06-26 13:03:45 +0200

    add empty user leaf

---
+++ Composite/Leaf/User.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @category    Pattern
+ * @package     Composite
+ * @subpackage  Leaf
+ * @author      Stefan Schwager
+ */
+
+namespace Pattern\Composite\Leaf;
+
+use Pattern\Composite\Component\UserComponent;
+
+class User extends UserComponent
+{
+}
